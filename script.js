e2d78d74eb8db11d1a7e31b2588f1740ee558fc1
denzsukks <daviddenzelmatthew.bsit@gmail.com>
2026-04-13 16:16:13 +0400

    Loading Animation

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+window.addEventListener("load", () => {
+  const loader = document.getElementById("loader");
+
+  setTimeout(() => {
+    loader.style.opacity = "0";
+    loader.style.transition = "0.5s ease";
+
+    setTimeout(() => {
+      loader.style.display = "none";
+    }, 500);
+  }, 1500); // duration of loader
+});
+
+
+
 console.log("Portfolio loaded 🚀");
